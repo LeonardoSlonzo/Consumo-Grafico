08b67df8306aac5707bea5ad60095558792b2f4c
LeonardoSlonzo <leonardoslonzo@hotmail.com>
2024-11-04 18:06:09 -0300

    Incial

---
+++ src/main/java/com/lslonzo/Consumo_Grafico/ConsumoGraficoApplication.java
@@ -0,0 +1,13 @@
+package com.lslonzo.Consumo_Grafico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoGraficoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoGraficoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lslonzo/Consumo_Grafico/ConsumoGraficoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lslonzo.Consumo_Grafico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoGraficoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
